fdba0fbe45fa420bfd418e826a93181a09da762e
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2023-07-06 19:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 //your JS code here. If required.
-let height = this.innerHeight;
-let width = this. innerWidth;
-let div=document.getElementById("sizeInfo");
-let h=div.firstchild;
-h.innerText = "Width:"+width "and Height:" + height;
-window.addEventListner("resize",function(){
-	let height = this.innerHeight;
-	let width = this. innerWidth;
-	let div=document.getElementById("sizeInfo");
-	let h=div.firstchild;
-	h.innerText = "Width:"+width "and Height:" + height;
-})
+// let height = this.innerHeight;
+// let width = this. innerWidth;
+// let div=document.getElementById("sizeInfo");
+// let h=div.firstchild;
+// h.innerText = "Width:"+width "and Height:" + height;
+// window.addEventListner("resize",function(){
+// 	let height = this.innerHeight;
+// 	let width = this. innerWidth;
+// 	let div=document.getElementById("sizeInfo");
+// 	let h=div.firstchild;
+// 	h.innerText = "Width:"+width "and Height:" + height;
+// })
+let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
 
 
