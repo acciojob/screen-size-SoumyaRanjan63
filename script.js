55c4d5193a6cf5cd867cee660c658ba6a76eeba3
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2023-07-06 19:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //your JS code here. If required.
-
-let height=window.innerHeight;
-    let width=window.innerWidth;
+let height=this.innerHeight;
+    let width=this.innerWidth;
    let div=document.getElementById("sizeInfo");
     let h=div.firstChild;
     h.innerText="Width: "+width+" and Height: "+height;  
 window.addEventListener("resize",function(){
-    let height=window.innerHeight;
-    let width=window.innerWidth;
+    let height=this.innerHeight;
+    let width=this.innerWidth;
    let div=document.getElementById("sizeInfo");
     let h=div.firstChild;
+ 
     h.innerText="Width: "+width+" and Height: "+height; 
+  
   })
-
-
